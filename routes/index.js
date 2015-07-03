878b4c0432157e6ca07809d164462c0bb2cbafd6
Carlos Cesar Barajas Jr <soldotsol@gmail.com>
2015-07-03 14:02:17 -0700

    New routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var SiteTitle = 'Rebirth Entertainment';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: SiteTitle });
+});
+
+/* GET home page. */
+router.get('/vision', function(req, res, next) {
+  res.render('vision', { title: SiteTitle });
+});
+
+/* GET home page. */
+router.get('/weddings', function(req, res, next) {
+  res.render('weddings', { title: SiteTitle });
+});
+
+/* GET home page. */
+router.get('/corporate-events', function(req, res, next) {
+  res.render('corporate-events', { title: SiteTitle });
+});
+
+/* GET home page. */
+router.get('/school-events', function(req, res, next) {
+  res.render('school-events', { title: SiteTitle });
 });
 
 module.exports = router;
